cd7d8ee48929117d02c9ccce7ee2e8af6e056b40
Mateus Almeida <165097236+mtgjbr@users.noreply.github.com>
2026-04-28 21:48:04 -0300

    inicio dos estudos e primeiro contato com html

---
+++ Aula01_Console.log/exercicio.js
@@ -0,0 +1,15 @@
+console.log('Ola,meu nome é "mateus". Estou aprendendo JavaScript  ás' ,10,'da manhã' )
+ /*
+  *
+  *
+  * 
+  * 
+  * 
+  * 
+  * Comentarios 
+  * 
+  * 
+  * 
+  * 
+  * 
+ */
---
+++ Aula01_Console.log/index.js
@@ -0,0 +1,9 @@
+//index.js = arquivo principal da pasta
+//console.log() = função para imprimir algo no console do navegador
+console.log("Olá mundo"); //string = texto, sempre entre aspas
+//objeto console e metodo log
+//objeto é uma coleção de propriedades e métodos ex: carro é um objeto, ele tem propriedades como cor, modelo, marca e métodos como ligar, acelerar, frear
+//propriedade = característica do objeto
+//método = ação que o objeto pode realizar 
+console.log(1234); //number = número, sem aspas
+console.log(15.85); 
