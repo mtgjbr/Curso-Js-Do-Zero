261b1f8ecec6be768862984319407a2d68558d2c
Mateus Almeida <165097236+mtgjbr@users.noreply.github.com>
2026-05-09 19:11:55 -0300

    Complemento aula 9

--- Aula_09_Operadores_De_Comparacao_E_Logicos/operadores_logicos.js
+++ Aula_09_Operadores_De_Comparacao_E_Logicos/operadores_logicos.js
@@ -17,3 +17,39 @@ console.log(vailogar);
 console.log(vailogarr) //nome diferente
 
 console.log(!true); //inverte o valor
+
+
+console.log( "luiz otavio" && 0 && "maria"); // retonra o considerado como falso 
+
+/**
+ *  Valores considerados como falso 
+ * false
+ * 0 
+ * " "
+ * null/ undefined
+ * NAM
+ */
+console.log("luiz" && "maria")
+
+function falaOi (){
+    return "oi";
+
+}
+
+const vaiExecutar = false;
+ 
+console.log(vaiExecutar && falaOi()); //como o valor é falso ele não continuou, o que fez a função não ser ativada
+console.log(true && falaOi()); // como o primeiro valor foi true ele continuou a acompanhar o circuito até chegar na função
+
+
+console.log( 0|| false || "Luiza mel" || true); //ele recebeu o primeiro true que ele pegou, no momento que ele pega o true ele para 
+
+let CorUsuario =null;
+let CorPadrao = CorUsuario||"preto";
+
+console.log( CorPadrao)
+ CorUsuario = "laranja" 
+ CorPadrao = CorUsuario||"preto"; // se não houvesse essa linha ele iria retorna preto, pois ele tirou "Uma foto" da variavel la em cima,e não pegou a formula 
+ console.log(CorPadrao)
+
+
