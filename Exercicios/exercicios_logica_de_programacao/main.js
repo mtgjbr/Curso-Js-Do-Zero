47ce5fd22795f2d848a66da586546cd28100f3d8
Mateus Almeida <165097236+mtgjbr@users.noreply.github.com>
2026-06-03 20:56:19 -0300

    Feat:ultima correção exercicio

--- Exercicios/exercicios_logica_de_programacao/main.js
+++ Exercicios/exercicios_logica_de_programacao/main.js
@@ -1,26 +1,26 @@
-//Atividade 5 — DOM simples
-//Crie uma página HTML com um input de texto e um botão. Quando o usuário clicar no botão, 
+    //Atividade 5 — DOM simples
+    //Crie uma página HTML com um input de texto e um botão. Quando o usuário clicar no botão, 
 
 
-// o texto digitado deve aparecer em um <p> na página. Se o campo estiver vazio, exibe "Digite algo primeiro!".
-const enviar = document.querySelector('.enviar');
-const input = document.querySelector ('.texto');
-const pai = document.querySelector('.pai');
+    // o texto digitado deve aparecer em um <p> na página. Se o campo estiver vazio, exibe "Digite algo primeiro!".
+    const enviar = document.querySelector('.enviar');
+    const input = document.querySelector ('.texto');
+    const pai = document.querySelector('.pai');
 
-function criaP(texto){ //cria um paragrafo e adiciona como filho do pai, adiciona o texto como o conteudo de p
- let p = document.createElement('p');
- pai.appendChild(p);
-  p.innerHTML = texto;
-}
+    function criaP(texto){ //cria um paragrafo e adiciona como filho do pai, adiciona o texto como o conteudo de p
+    let p = document.createElement('p');
+    pai.appendChild(p);
+    p.innerHTML = texto;
+    }
 
 
-enviar.addEventListener('click',function(e){ // se estiver algo escrito , pega o valor e chama o criap
-    if(input.value){
-   const texto = input.value;
-   input.value = " "
-   criaP(texto) 
-}else //caso o contrario escreve a frase insira um texto
-     
-    criaP("insira um texto")
-})
+    enviar.addEventListener('click',function(e){ // se estiver algo escrito , pega o valor e chama o criap
+        if(input.value){
+    const texto = input.value;
+    input.value = ""
+    criaP(texto) 
+    }else{ //caso o contrario escreve a frase insira um texto
+        criaP("insira um texto")
+    }
+        })
 
