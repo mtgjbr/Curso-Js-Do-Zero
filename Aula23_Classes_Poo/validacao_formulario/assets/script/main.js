615e2fc698f8ba6a6256a1cb44a3a9758a51b471
Mateus Almeida <165097236+mtgjbr@users.noreply.github.com>
2026-07-02 20:43:47 -0300

    Feat:revisao atividade formulario

--- Aula23_Classes_Poo/validacao_formulario/assets/script/main.js
+++ Aula23_Classes_Poo/validacao_formulario/assets/script/main.js
@@ -1,12 +1,13 @@
 class ValidaFormulario {
     constructor() {
         this.formulario = document.querySelector('.formulario')
+        this.eventos(); // já chama eventos quando cria o objeto
 
-        this.eventos();
     }
     eventos() {
-        this.formulario.addEventListener('submit', e => { // usa arrow function para não perder o this, pq se nao o this iria ser alterado para:?
-            this.handleSubmit(e);
+        this.formulario.addEventListener('submit', e => { // Se usasse function normal, o this dentro dela apontaria pro elemento que disparou o evento (o formulário), não pra classe.
+
+            
         });
     }
 
@@ -15,7 +16,7 @@ class ValidaFormulario {
         const camposvalidos = this.camposSaoValidos();
         const senhasValidas = this.senhasSaoValidas();
         console.log(camposvalidos, senhasValidas)
-        if(camposvalidos && senhasValidas) {
+        if(camposvalidos && senhasValidas) { // se os dois forem true envia
             alert("formulario enviado");
             this.formulario.submit()
         }
@@ -28,8 +29,7 @@ class ValidaFormulario {
 
         if(senha.value !== repetirSenha.value){
             valid = false
-            this.criaErro(senha, "campo senha e repetir senha precisam ser iguais")// como que o js sabe que a variavel sena vai dar no input senha?
-            this.criaErro(repetirSenha, "campo senha e repetir senha precisam ser iguais")
+            this.criaErro(senha, "campo senha e repetir senha precisam ser iguais")
 
         }
           if(senha.value.length <6 || senha.value.length > 12){
@@ -44,8 +44,8 @@ class ValidaFormulario {
 
     camposSaoValidos() {
         let valid = true; // o formulario começa true
-        for (let errorText of this.formulario.querySelectorAll(".error-text")) {// cria a variavel errorText e da pra ela o valor de cada errortext, e remove ela
-            errorText.remove();
+        for (let errorText of this.formulario.querySelectorAll(".error-text")) {// cria a variavel errorText e da pra ela o valor de cada errortext, serve para remover erros antigos
+            errorText.remove(); 
         }
         
         for (let campo of this.formulario.querySelectorAll('.validar')) { //pega todos os inputs pelas classes validar
@@ -68,7 +68,7 @@ class ValidaFormulario {
     }
     validaUsuario(campo){
         const usuario = campo.value;
-        let valid= true //porque ele teve que criar outra variavel vlaid?
+        let valid= true //porque ele teve que criar outra variavel vlaid? não preciava, só ter retornado false ou true
         if(usuario.length<3 ||usuario.length >12){
             this.criaErro(campo,"Usuário precisa ter entre 3 e 12 caracteres")
              return valid = false
@@ -81,7 +81,7 @@ class ValidaFormulario {
     }
 
     validaCPF(campo) {
-        const cpf = new validaCpf(campo.value);
+        const cpf = new validaCpf(campo.value); //Tudo que é declarado num arquivo fica disponível globalmente pros outros —
         if (!cpf.valida()) {
             this.criaErro(campo, "Cpf inválido")
             return false
@@ -91,7 +91,7 @@ class ValidaFormulario {
 
     criaErro(campo, msg) {
         const div = document.createElement('div');
-        div.innerHTML = msg;
+        div.innerHTML = msg; // cria uma div com a mensagem de erro e coloca apos o fim do campo
         div.classList.add('error-text');
         campo.insertAdjacentElement('afterend', div)
     }
