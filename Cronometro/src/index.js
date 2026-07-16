1fb06bee752f354fe2eb3e53a156585862bd3292
Mateus Almeida <165097236+mtgjbr@users.noreply.github.com>
2026-07-15 21:28:45 -0300

    Feat:termino do cronometro com voltas e pausar

--- Cronometro/src/index.js
+++ Cronometro/src/index.js
@@ -1,26 +1,32 @@
 import './assets/css/style.css'
+import Cronômetro from './modules/cronometro';
 // Cronômetro com "Voltas" (Laps): Expanda o cronômetro que você estudou
 //Adicione um botão "Volta" que cria uma lista de marcadores de tempo no DOM, praticando a
 //  criação dinâmica de elementos HTML
-const relogio = document.querySelector(".relogio");
+
+const comecar = document.querySelector('.comecar');
+const pausar = document.querySelector('.pausar');
+const volta = document.querySelector('volta');
+const reiniciar = document.querySelector('.reiniciar');
+
 //let timer = "";
 let segundos = 0;
+const c1= new Cronômetro();
 
-function CriaHoraDosSegundos(segundos) {
-    const data = new Date(segundos * 1000); // adiciona 1 seg na hora zero a cada repetição do laço
-    // como o date corrige automaticamente de 60 segs para 1 min, não há necessidade de nenhum tipo de correção manual
+document.addEventListener('click',(e)=>{
+ console.log(e.target)
+ if(e.target.classList.contains("comecar")){  // confere se nas classes do botão tem começar
+    c1.comecar()}
+  if(e.target.classList.contains("pausar")){ 
+    c1.pausar()}
+ if(e.target.classList.contains("volta")){ 
+    c1.marcarVolta()}
+    if(e.target.classList.contains("reinciar")){ 
+    c1.reinicar()}
 
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false, // formato 00:00:00 até 24 hrs
-        timeZone: 'gmt' //considerando 01/01/1970 pegando a hora e minuto 0
-    });
-}
+})
 
-let timer = setInterval(() => { // se eu passar direto o interval vai vir o id pra cancelar ele com timeout
-    let horaFormatada = CriaHoraDosSegundos(segundos);
-    segundos++
-    relogio.innerHTML = horaFormatada;
-}, 1000);
+
 
 
 
--- Cronometro/src/modules/cronometro.js
+++ Cronometro/src/modules/cronometro.js
@@ -1,18 +1,77 @@
 
+const relogio = document.querySelector(".relogio");
+const voltas = document.querySelector(".voltas")
 
-class Cronômetro{
-    constructor(){
-     let segundos = 0;
+export default class Cronômetro {
+    constructor() {
+        this.segundos = 0; // se colocasse o data aquiCalculou a hora uma única vez quando o objeto foi criado e guardou o texto "00:00:00" dentro de this.data
+        //Se você colocasse o valor 1000 diretamente no new Date(1000),
+        // o JavaScript criaria um objeto de data representando exatamente 1 segundo após o marco zero da época Unix (01/01/1970)
+        this.horaFormatada = 0
+        this.contador = null // todo setinterval ganha um id, ele verifica ele já tem esse id no if
+        this.posicao = 1
     }
-pausar(){
+    criali(){
+     const li =document.createElement("li");
+     return li
+    }
+    atualizarTela(hora) {
+        relogio.innerHTML = hora;
+    }
 
+    criaRelogio() {
+        const data = new Date();
+    }
+
+    pausar() {
+        clearInterval(this.contador)
+        this.contador = null;
+    }
+
+    marcarVolta() {
+        if(this.segundos===0){
+            alert("comece para adicionar voltas");
+        }
+        else{
+
+            const li = this.criali();
+            li.textContent = `${this.posicao}º: ${this.horaFormatada}`;
+            voltas.appendChild(li);
+            console.log(this.horaFormatada);
+            this.posicao++
+
+        }
+
+    }
+    comecar() {
+        if (this.contador) { alert("voce já começou a fazer isso") }
+        else {
+            this.contador = setInterval(() => {
+                let data = new Date(this.segundos * 1000);
+                data = data.toLocaleTimeString('pt-Br', {
+                    hour12: false,
+                    timeZone: "utc"
+                })
+                this.horaFormatada = data
+                this.atualizarTela(this.horaFormatada)
+                // console.log(data)
+                this.segundos++
+            }, 1000);
+
+        }
+    }
+
+    reinicar(){
+        this.segundos = 0;
+        this.pausar()
+        this.atualizarTela("00:00:00");
+        voltas.replaceChildren();
+        this.posicao =1
+
+        
+       
+       
+       
+    }
 }
-marcarVolta(){
 
-}
-comecar(){
-
-}
-
-
-}
